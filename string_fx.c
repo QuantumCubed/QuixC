71a70f339ff7cac936a391eaf0c12f1cca78eb31
Anish Kurani <anish.kurani09@gmail.com>
2025-12-10 22:32:51 -0500

    added heap implementation for CharArray type

--- string_fx.c
+++ string_fx.c
@@ -1,17 +1,24 @@
 #include <stdio.h>
 #include <ctype.h>
 #include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <stddef.h>
 
-// #define str(buffer, buffer_size) (String) { .chars = buffer, .length = strlen(buffer), .capacity = buffer_size }
-#define str(BUFFER_SIZE) (String) { .chars = buffer, .length = strlen(buffer), .capacity = BUFFER_SIZE }
-typedef struct String {
+#define str(BUFFER_SIZE) (Str) { .chars = (char[BUFFER_SIZE]){0}, .length = 0, .capacity = BUFFER_SIZE, ._heap_allocated = false }
+
+typedef struct CharArray {
 	char *chars;
 	size_t length; // DOES NOT INCLUDE NULL TERMINATOR: '\0'
 	size_t capacity; // size (in bytes) the buffer can hold
-} String;
+	bool _heap_allocated;
+} CharArray;
 
-void trim_leading_whitespace(String *str) {
-	int index = 0;
+typedef CharArray Str;     // Documents: stack-based string
+typedef CharArray String;  // Documents: heap-based string
+
+void trim_leading_whitespace(CharArray *str) {
+	size_t index = 0;
 	while(index < (str -> length) && isspace((str -> chars)[index])) {
 		++index;
 	}
@@ -19,8 +26,8 @@ void trim_leading_whitespace(String *str) {
 	str -> length = (str -> length) - index;
 }
 
-void trim_trailing_whitespace(String *str) {
-	int index = (str -> length) - 1;
+void trim_trailing_whitespace(CharArray *str) {
+	size_t index = (str -> length) - 1;
 	while(index >= 0 && isspace((str -> chars)[index])) {
 		--index;
 	}
@@ -28,8 +35,8 @@ void trim_trailing_whitespace(String *str) {
 	str -> length = index + 1;
 }
 
-void trim_all_whitespace(String *str) {
-	int read, write = 0;
+void trim_all_whitespace(CharArray *str) {
+	size_t read, write = 0;
 	while(read < str -> length) {
 		if(!isspace((str -> chars)[read])) {
 			(str -> chars)[write] = (str -> chars)[read];
@@ -41,31 +48,76 @@ void trim_all_whitespace(String *str) {
     str -> length = write;
 }
 
-// void concat(String *self, const char *str) {
-// 	int new_length = (self -> length) + 
-// }
+void str_write(CharArray *self, const char *str) {
+	size_t i = 0;
+	while(str[i] != '\0' && i < (self -> capacity) - 1) {
+		(self -> chars)[i] = str[i];
+		++i;
+	}
+	(self -> chars)[i] = '\0';
+	self -> length = i;
+	if(str[i] != '\0') {
+		printf("BUFFER LIMIT EXCEEDED; TRUNCATING...\n");
+	}
+}
 
-// void update_chars(String *self, const char *str) {
-// 	int i = 0;
-// 	while(str[i] != '\0' && i < (self -> capacity) - 1) {
-// 		(self -> chars)[i] = str[i];
-// 		(self -> length) += 1;
-// 		++i;
-// 	}
-// }
+void concat(CharArray *self, const char *str) {
+	size_t i = self -> length;
+	size_t j = 0;
+
+	while(str[j] != '\0' && i < (self -> capacity) - 1) {
+		(self -> chars)[i] = str[j];
+		++i;
+		++j;
+	}
+	(self -> chars)[i] = '\0';
+	self -> length = i;
+	if(str[j] != '\0') {
+		printf("BUFFER LIMIT EXCEEDED; TRUNCATING...\n");
+	}
+}
+
+static inline String *string(size_t BUFFER_SIZE) {
+	String *s = (String *) malloc(sizeof(String));
+	if(!s) {
+		fprintf(stderr, "String Alloc ERR!\n");
+	}
+	s -> chars = (char *) malloc(sizeof(char) * BUFFER_SIZE);
+	if(!s -> chars) {
+		free(s);
+		fprintf(stderr, "Chars Alloc ERR!\n");
+	}
+	s -> length = 0;
+	s -> capacity = BUFFER_SIZE;
+	s -> _heap_allocated = true;
+	return s;
+}
+
+void string_cleanup(String *self) {
+	if (self -> _heap_allocated) {
+		free(self -> chars);
+		free(self);
+	}
+}
 
 int main(void) {
-	// char buffer[] = "   This is a string with spaces   ";
-	char buffer[3] = {0};
-	String newStr = str();
-	// String newStr = str("This is a string")
-	// String newStr = str(BUFFER[BUFFER_SIZE])
-	// update_chars(&newStr, "a");
-	// trim_leading_whitespace(&newStr);
-	// trim_trailing_whitespace(&newStr);
-	// trim_all_whitespace(&newStr);
+	// Str newStr = str(10);
+	// str_write(&newStr, "abc");
+	// concat(&newStr, "def");
+	// concat(&newStr, "ghi");
 
-	puts(newStr.chars);
-	printf("%zu\n", newStr.length);
+	// puts(newStr.chars);
+	// printf("%zu\n", newStr.length);
+
+	// String *newStr2 = string(10);
+	// str_write(newStr2, "abc");
+	// concat(newStr2, "def");
+	// concat(newStr2, "ghi");
+
+	// puts(newStr2 -> chars);
+	// printf("%zu\n", newStr2 -> length);
+
+	// string_cleanup(newStr2);
+
 	return 0;
 }
